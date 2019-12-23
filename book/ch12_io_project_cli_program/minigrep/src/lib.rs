5df849f35217faf59ae05fdf6fa66debbc01a728
Adeynack <Adeynack@users.noreply.github.com>
2019-12-22 23:19:24 -0500

    12.3 implement the search function

--- book/ch12_io_project_cli_program/minigrep/src/lib.rs
+++ book/ch12_io_project_cli_program/minigrep/src/lib.rs
@@ -3,6 +3,11 @@ use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     Ok(())
 }
 
@@ -24,7 +29,15 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
