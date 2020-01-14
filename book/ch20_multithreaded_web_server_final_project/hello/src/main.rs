556dba95fcf32f5f779cf500b6e0d6c3b8037025
Adeynack <Adeynack@users.noreply.github.com>
2020-01-14 06:53:42 +0100

    ch20 Writing a Response

--- book/ch20_multithreaded_web_server_final_project/hello/src/main.rs
+++ book/ch20_multithreaded_web_server_final_project/hello/src/main.rs
@@ -16,4 +16,8 @@ fn handle_connection(mut stream: TcpStream) {
     println!();
     println!("-----====[ Request ]====-----");
     println!("{}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
