90a96cc43fa666f16beed1ebd9d126a22e73755a
Adeynack <Adeynack@users.noreply.github.com>
2020-01-14 10:42:52 +0100

    ch20 share a channel between the workers

--- book/ch20_multithreaded_web_server_final_project/hello/src/lib.rs
+++ book/ch20_multithreaded_web_server_final_project/hello/src/lib.rs
@@ -1,7 +1,9 @@
 use std::thread;
+use std::sync::{mpsc, Arc, Mutex};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -14,14 +16,19 @@ impl ThreadPool {
     /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         ThreadPool {
-            workers
+            workers,
+            sender,
         }
     }
 
@@ -36,9 +43,13 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let handle = thread::spawn(|| {});
 
         Worker { id, handle }
     }
+}
+
+struct Job {
+
 }
