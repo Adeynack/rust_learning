a7dd987d1f34987d14ce2611c08819237e6cd954
Adeynack <Adeynack@users.noreply.github.com>
2020-01-14 07:08:09 +0100

    ch20 Returning Real HTML

--- book/ch20_multithreaded_web_server_final_project/hello/src/main.rs
+++ book/ch20_multithreaded_web_server_final_project/hello/src/main.rs
@@ -1,5 +1,9 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::fs;
+
+const LOG_REQUESTS: bool = true;
+const LOG_RESPONSES: bool = true;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -13,11 +17,23 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!();
-    println!("-----====[ Request ]====-----");
-    println!("{}", String::from_utf8_lossy(&buffer[..]));
+    if LOG_REQUESTS {
+        println!();
+        println!("-----====[ Request ]====-----");
+        println!("{}", String::from_utf8_lossy(&buffer[..]));
+    }
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write(response.as_bytes()).unwrap();
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    if LOG_RESPONSES {
+        println!();
+        println!("-----====[ Response ]====-----");
+        println!("{}", response);
+    }
+
+    let written = stream.write(response.as_bytes()).unwrap();
+    println!("Written {} bytes to response", written);
+
     stream.flush().unwrap();
 }
